923ee54dd4c1b328ba369e3a225d456963787f0f
SHIELD7 <menghehe@live.cn>
2019-06-20 20:06:38 +0800

    中文乱码修复

--- app/src/main/java/site/imcu/weibo/net/CommentService.java
+++ app/src/main/java/site/imcu/weibo/net/CommentService.java
@@ -8,12 +8,14 @@ import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.HeaderMap;
+import retrofit2.http.Headers;
 import retrofit2.http.POST;
 import retrofit2.http.Query;
 import rx.Observable;
 
 public interface CommentService {
     @FormUrlEncoded
+    @Headers("Content-Type:application/x-www-form-urlencoded; charset=utf-8")
     @POST("/comment")
     Observable<JSONObject> postComment(@HeaderMap Map<String,String> headers, @Field("commentContent")String commentContent,@Field("weiboId")int weiboId);
 
